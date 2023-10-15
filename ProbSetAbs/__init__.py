ed4fe5babb8c8275aa7a40622932b0efa7018242
KOLANICH <kolan_n@mail.ru>
2023-10-15 23:04:19 +0300

    We had to move to Codeberg

--- ProbSetAbs/__init__.py
+++ ProbSetAbs/__init__.py
@@ -1,2 +1,6 @@
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KAbs/ProbSetAbs.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
+
 from . import backends
 from .core import getAllBackends, getBackend
